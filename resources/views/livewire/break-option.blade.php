c0a376407218a2d6dddfe0219af875a49bedb8d7
Trent Johnson <36277083+trent-johnson@users.noreply.github.com>
2022-08-24 17:59:06 -0400

    Swapping from anchors to buttons for modal.

--- resources/views/livewire/break-option.blade.php
+++ resources/views/livewire/break-option.blade.php
@@ -27,10 +27,10 @@
                     </svg>
                     {{(array_key_exists('playingtime',$game)) ? $game['playingtime']['@attributes']['value'] . 'm' : 'N/A' }}</div>
                 <div class="flex-auto bg-slate-300 rounded text-gray-500  px-2 py-1 text-sm mb-2">
-                    <a href="#" data-modal-toggle="{{ $thing_id }}-modal">
+                    <button type="button" data-modal-toggle="{{ $thing_id }}-modal">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
-                        </svg> Details</a>
+                        </svg> Details</button>
 
                     <div id="{{ $thing_id }}-modal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
                         <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
@@ -54,7 +54,7 @@
                                 </div>
                                 <!-- Modal footer -->
                                 <div class="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200">
-                                    <a href="#" data-modal-toggle="{{ $thing_id }}-modal" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">Close</a>
+                                    <button type="button" data-modal-toggle="{{ $thing_id }}-modal" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">Close</button>
                                     <a href="https://boardgamegeek.com/boardgame/{{ $thing_id }}" target="_blank" class="text-white bg-indigo-700 hover:bg-indigo-800 focus:ring-4 focus:outline-none focus:ring-indigo-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">View More Details</a>
                                 </div>
                             </div>
